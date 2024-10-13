4fcd90d950d202529fe8b4981dd229480a95d412
Olaoluwa  Ayidina <ayidinao@gmail.com>
2024-10-13 06:16:05 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-<<<<<<< HEAD
    //base: "/Fleet-management/",
 });
-=======
-  // base: "/Fleet-manageent/",
-});
->>>>>>> 8c5d6cc3f5927405a08ea8bc077c30d218f37b8d
