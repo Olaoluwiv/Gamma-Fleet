3a509fb88b21879fda9124c030037bd091c2b0b8
Olaoluwa  Ayidina <132825905+Olaoluwiv@users.noreply.github.com>
2024-10-14 06:14:47 +0100

    Updated vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,9 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-
 export default defineConfig(({ command, mode }) => {
   return {
     plugins: [react()],
-    base: command === 'build' ? '/Fleet-management/' : '/', // Use '/' for local dev and '/Fleet-management/' for GitHub Pages
+    base: command === 'build' ? '/Fleet-management/' : '/', // Use '/' for local dev and '/Fleet-managemenlsnpmt/' for GitHub Pages
   };
 });
