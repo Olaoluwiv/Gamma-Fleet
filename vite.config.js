9d3ef03b9ee6a843d5da6b03e46bdb72359581b5
Olaoluwa  Ayidina <ayidinao@gmail.com>
2024-10-17 16:46:47 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ command, mode }) => {
   return {
     plugins: [react()],
-    base: command === 'build' ? '/Gamma-Fleet/' : '/', // Use '/' for local dev and '/Gamma-Fleet/' for GitHub Pages
+    base: command === 'build' ? '/Gamma-Fleet/' : '/', // Use '/Gamma-Fleet/' for GitHub Pages
   };
 });
