76eab4934eb9a366ea8afab74e6828184372f68a
Olaoluwa  Ayidina <132825905+Olaoluwiv@users.noreply.github.com>
2024-10-17 18:20:22 +0100

    update

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from "@vitejs/plugin-react";
 export default defineConfig(({ command, mode }) => {
   return {
     plugins: [react()],
-    base: command === 'build' ? '/Gamma-Fleet/' : '/', // Use '/Gamma-Fleet/' for GitHub Pages
+    base: command === 'build' ? '/Fleet-management/' : '/', 
   };
 });
